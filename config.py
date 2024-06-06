df051fc3f8101c0290800dfee0c70c0c0a6e28d9
happy0088 <abhishek.k0088@gmail.com>
2024-06-07 00:30:44 +0530

    Update config with Postman URL and headers
    Signed-off-by: happy0088 <abhishek.k0088@gmail.com>

--- config.py
+++ config.py
@@ -11,3 +11,22 @@ EMAIL_CONFIG = {
 }
 
 QUERY_TEXT = 'YourCompanyName'
+API_URL = 'https://www.postman.com/_api/ws/proxy'
+HEADERS = {
+    'Host': 'www.postman.com',
+    'Content-Length': '327',
+    'Sec-Ch-Ua': '"Chromium";v="123", "Not:A-Brand";v="8"',
+    'Sec-Ch-Ua-Platform': '"macOS"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.58 Safari/537.36',
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'Origin': 'https://www.postman.com',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://www.postman.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'Priority': 'u=1, i'
+    }
