7cba8fbe99846d8f1e5758830aef86ce5c81bc5c
happy0088 <abhishek.k0088@gmail.com>
2024-06-07 17:04:50 +0530

    parsed data to json before loading into database
    Signed-off-by: happy0088 <abhishek.k0088@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from services.leak_detection.postman_leak_detector import PostmanLeakDetector
 from utils.data_parser import Parser
@@ -21,25 +22,36 @@ def main():
 
         if response:
             try:
-                parsed_data = Parser.parse_response(response)
+                parsed_data_list = Parser.parse_response(response)
+                parsed_response_data_list = json.dumps(parsed_data_list['data'])
+                parsed_response_data_list = json.loads(parsed_response_data_list)
             except Exception as e:
                 logging.error(f"Error parsing response: {e}")
-                parsed_data = None
+                parsed_data_list = []
 
-            if parsed_data:
+            if parsed_data_list:
                 db_service = DBService()
-                try:
-                    if db_service.find_new_data(parsed_data):
-                        db_service.insert_data(parsed_data)
-                except Exception as e:
-                    logging.error(f"Error handling database operations: {e}")
+                notification_service = NotificationService()
+                
+        for parsed_data_str in parsed_response_data_list:
+            try:
+                # Parse each JSON string into a dictionary
+                parsed_data = (parsed_data_str)
 
-                try:
-                    notification_service = NotificationService()
-                    message = f"New findings: {parsed_data}"
+                # Serialize the data field as JSON
+                parsed_data_json = json.dumps(parsed_data["document"])
+                parsed_data_json = json.loads(parsed_data_json)
+
+                if db_service.find_new_data(parsed_data_json):
+                    # Insert the data into the database
+                    db_service.insert_data(parsed_data_json)
+                                    
+                    # Send notification for each inserted row
+                    message = f"New finding: {parsed_data}"
                     notification_service.send_notification(message)
-                except Exception as e:
-                    logging.error(f"Error sending notification: {e}")
+            except Exception as e:
+                logging.error(f"Error processing data: {e}")
+
 
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
